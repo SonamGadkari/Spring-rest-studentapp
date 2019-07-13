6cfb97e6b510a219b4e9d13fc8f5ff33747d50b5
Sonam Gadekari <S533490@nwmissouri.edu>
2019-07-13 13:49:56 -0500

    Get students by Id ,data is seeded using postconstruct

--- src/main/java/studentApp/StudentController.java
+++ src/main/java/studentApp/StudentController.java
@@ -3,6 +3,8 @@ package studentApp;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -11,24 +13,25 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/api")
 public class StudentController {	
-
-	@GetMapping("/students")
-	public List<Student> getStudents()
+	private List<Student> students;
+	
+	@PostConstruct
+	public void loadStudentData()
 	{
-		List<Student> students=new ArrayList<Student>();
+		students=new ArrayList<Student>();
 		students.add(new Student("Sonam","Gadekari"));
 		students.add(new Student("Suman","Gadekari"));
-		students.add(new Student("Rohith","Gadekari"));				
+		students.add(new Student("Rohith","Gadekari"));		
+	}
+	@GetMapping("/students")
+	public List<Student> getStudents()
+	{			
 		return students;
 	}
 	
 	@GetMapping("/students/{studentId}")
 	public Student getStudents(@PathVariable int studentId)
-	{
-		List<Student> students=new ArrayList<Student>();
-		students.add(new Student("Sonam","Gadekari"));
-		students.add(new Student("Suman","Gadekari"));
-		students.add(new Student("Rohith","Gadekari"));				
+	{				
 		return students.get(studentId);
 	}
 }
